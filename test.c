f460f26a38d49e6362b7260c64dec8df86a06122
Dwoe4ka~ <sandrij.prof@gmail.com>
2023-10-04 17:15:42 +0300

    It still does not work and i want to cry

--- test.c
+++ test.c
@@ -2,30 +2,8 @@
 #include<stdlib.h>
 #include<conio.h>
 #include<string.h>
-
+//awesome
 int main() {
- char str[80];
- char key[20];
- printf("ENTER TEXT YOU WANT TO ENCODE: ");
- fgets(str, 80,stdin);
- printf("ENTER KEY: ");
- fgets(key, 20,stdin);
- int ss = strlen(str);
- int sk = strlen(key);
- char a = 0, b = 0, c;
- printf("ENDCODED MESSAGE: ");
- for (int i = 0, j = 0; i<ss-1; i++,j++) {
-    if (j==sk-1) {
-    j = 0;
-    }
-    if (ss==j+1) {
-    i = ss+1;
-    }
-    a = str[i];
-    b = key[j];
-    c=a^b;
-    printf("%c",c);
-  }
-getch();
+printf("Test String");
 return 0;
 }
