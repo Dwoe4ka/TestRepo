a8b682580a4d97aa3a1f016b1b2b089bb650e3a9
Dwoe4ka <sandrij.prof@gmail.com>
2023-10-06 11:26:37 +0300

    This is a calculator now

--- test.c
+++ test.c
@@ -1,10 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-#include<string.h>
-//awesome
+#include <stdio.h>
+
 int main() {
-printf("Slava Ukraini\n");
-printf("Geroyam slava");
-return 0;
+    double num1, num2, result;
+    char operator;
+
+    printf("Введіть перше число: ");
+    scanf("%lf", &num1);
+
+    printf("Введіть операцію (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Введіть друге число: ");
+    scanf("%lf", &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                printf("Помилка: Ділення на нуль неможливе.\n");
+                return 1; // Повертаємо 1, щоб позначити помилку
+            }
+            break;
+        default:
+            printf("Помилка: Непідтримувана операція.\n");
+            return 1; // Повертаємо 1, щоб позначити помилку
+    }
+
+    printf("Результат: %.2lf\n", result);
+
+    return 0; // Повертаємо 0, щоб позначити успішне завершення програми
 }
